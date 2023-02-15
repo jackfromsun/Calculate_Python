712852c45bd60f876d5b4a040702a773da121c78
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 11:17:25 +0530

    Create CalculateBattingAverage.py

---
+++ CalculateBattingAverage.py
@@ -0,0 +1,6 @@
+runs_scored = int(input("Enter the total number of runs scored by the player: "))
+times_out = int(input("Enter the total number of times the player has been out: "))
+
+batting_average = runs_scored / times_out
+
+print("The batting average of the player is:", batting_average)
