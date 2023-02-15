f2d05d634f6432028f39581ef15f93eee7486543
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 11:15:21 +0530

    Create CalculateCommissionPercentage.py

---
+++ CalculateCommissionPercentage.py
@@ -0,0 +1,6 @@
+commission_earned = float(input("Enter the commission earned: "))
+total_sale = float(input("Enter the total sale amount: "))
+
+commission_percentage = (commission_earned / total_sale) * 100
+
+print("The commission percentage is:", commission_percentage, "%")
